2e7514287b472e64db6931e34e21601db8050462
Huỳnh Nguyễn Quốc Bảo <hnquocbao22@gmail.com>
2023-04-15 11:42:24 +0700

    init

---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
+
+const Profile: React.FC = () => {
+  return <PageTitle>COn CAc Beo</PageTitle>;
+};
+
+export default Profile;
